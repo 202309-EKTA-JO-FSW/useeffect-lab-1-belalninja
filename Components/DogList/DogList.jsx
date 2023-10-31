c24a7bfc1b4a3d3cfc4b91ff797131c621db2abc
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2023-10-31 07:28:33 +0200

    Done

--- Components/DogList/DogList.jsx
+++ Components/DogList/DogList.jsx
@@ -1,17 +1,14 @@
-
-
 export default function ShoppingList({ dogsList = [] }) {
+  const listing = dogsList.map((dog, index) => (
+    <img src={dog} alt="" key={index} />
+  ));
   return (
     <div className="shopping-cart">
       <h3 style={{ textAlign: "center" }}>
         Cute Dogs
-        <span role="img">
-          🐶
-        </span>
+        <span role="img">🐶</span>
       </h3>
-      {dogsList.map((dog) => (
-        <img src={dog} alt="" />
-      ))}
+      {listing}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,35 @@
-import DogList from '../Components/DogList/DogList'
-import Form from '../Components/Form/Form'
-import { Inter } from 'next/font/google'
+import DogList from "../Components/DogList/DogList";
+import Form from "../Components/Form/Form";
+import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   // You will need to put a state here to save all the dogs data into
   // And you will fetch the data with useEffect
+  const [dogsList, setDogsList] = useState([]);
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+      .then((res) => res.json())
+      .then((list) => setDogsList(list.message));
+  }, []);
 
-   return (
+  function handleSubmit(num) {
+    fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+      .then((res) => res.json())
+      .then((list) => setDogsList(list.message));
+  }
+
+  return (
     <div className="card">
       {/* When the button is clicked in the form, it should fetch the information. 
           How can we do that by utilizing useState?
           
       */}
-      {/* <Form /> Uncomment <Form /> if you are going after the bonus */}
+      <Form setNumberOfDogs={handleSubmit} />
       {/* This page should receive the images array */}
-      <DogList />
+      <DogList dogsList={dogsList} />
     </div>
   );
 }
-
